888d70e440dd4e97e6a982be455d4ee48478c82c
Mario Ynocente Castro <ycmario@gmail.com>
2016-12-12 07:36:09 +0100

    add labels to first plot

--- plot_atari.py
+++ plot_atari.py
@@ -5,12 +5,19 @@ import common.file_utils as file_utils
 stats = file_utils.load_stats('SARSALambda-stats.npz')
 
 f, axarr = plt.subplots(4, sharex=True)
-axarr[0].plot(stats['rewards'])
-axarr[0].plot(stats['avg_rewards_all'])
-axarr[0].plot(stats['avg_rewards_partial'])
+
+rewards, = axarr[0].plot(stats['rewards'], label='reward')
+avg_all, = axarr[0].plot(stats['avg_rewards_all'], label='average(all)')
+avg_partial, = axarr[0].plot(stats['avg_rewards_partial'], label='average(last 50)')
+axarr[0].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=3, mode="expand", borderaxespad=0.)
+
 axarr[1].plot(stats['dict_sizes'])
+
 axarr[2].plot(stats['min_weights'])
 axarr[2].plot(stats['max_weights'])
 axarr[2].plot(stats['avg_weights'])
+
 axarr[3].plot(stats['num_frames'])
+
 plt.show()
