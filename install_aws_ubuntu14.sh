9229de408a4537d3e4aef2d0d050a731a48b2606
marioyc <ycmario@gmail.com>
2016-12-22 17:21:53 +0000

    add OpenCV installation

--- install_aws_ubuntu14.sh
+++ install_aws_ubuntu14.sh
@@ -5,6 +5,7 @@ apt-get install -y python2.7-dev python-pip
 
 pip install numpy
 
+# install ALE
 sudo apt-get install -y libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
 git clone https://github.com/mgbellemare/Arcade-Learning-Environment.git ~/ALE
 cd ~/ALE
@@ -13,3 +14,13 @@ cd build
 cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF -DBUILD_EXAMPLES=ON ..
 make -j 4
 pip install ..
+
+# install OpenCV
+git clone https://github.com/Itseez/opencv.git ~/opencv
+cd ~/opencv
+git checkout 3.1.0
+mkdir build
+cd build
+cmake .. -DBUILD_opencv_python2=ON
+make -j 4
+make install
