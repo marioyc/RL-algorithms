52388996a4cba3c803a7d74706624565330f7641
Mario Ynocente Castro <ycmario@gmail.com>
2016-12-23 14:36:54 +0100

    divide in two plots

--- plot_atari.py
+++ plot_atari.py
@@ -7,22 +7,26 @@ AGENT = 'SARSALambda'
 
 stats = file_utils.load_stats('{}-{}.npz'.format(GAME, AGENT))
 
-f, axarr = plt.subplots(4, sharex=True)
+f, axarr = plt.subplots(2, sharex=True)
 
-rewards, = axarr[0].plot(stats['rewards'], label='reward')
+#rewards, = axarr[0].plot(stats['rewards'], label='reward')
 avg_all, = axarr[0].plot(stats['avg_rewards_all'], label='all')
 avg_partial, = axarr[0].plot(stats['avg_rewards_partial'], label='last 50')
 legend1 = axarr[0].legend(bbox_to_anchor=(1, 1), loc=2, ncol=1,prop={'size':10})
 
-axarr[1].plot(stats['dict_sizes'])
+#axarr[1].plot(stats['num_frames'], label='frames')
+axarr[1].plot(stats['avg_frames_all'], label='all')
+axarr[1].plot(stats['avg_frames_partial'], label='last 50')
+legend2 = axarr[1].legend(bbox_to_anchor=(1, 1), loc=2, ncol=1,prop={'size':10})
 
-axarr[2].plot(stats['min_weights'])
-axarr[2].plot(stats['max_weights'])
-axarr[2].plot(stats['avg_weights'])
+f.savefig('stats/' + GAME + '_' + AGENT + '_rewards_frames.png', bbox_extra_artists=(legend1, legend2), bbox_inches='tight')
 
-axarr[3].plot(stats['num_frames'], label='frames')
-axarr[3].plot(stats['avg_frames_all'], label='all')
-axarr[3].plot(stats['avg_frames_partial'], label='last 50')
-legend2 = axarr[3].legend(bbox_to_anchor=(1, 1), loc=2, ncol=1,prop={'size':10})
+f, axarr = plt.subplots(2, sharex=True)
 
-f.savefig('stats/' + GAME + '_' + AGENT + '.png', bbox_extra_artists=(legend1, legend2), bbox_inches='tight')
+axarr[0].plot(stats['dict_sizes'])
+
+axarr[1].plot(stats['min_weights'])
+axarr[1].plot(stats['max_weights'])
+axarr[1].plot(stats['avg_weights'])
+
+f.savefig('stats/' + GAME + '_' + AGENT + '_weights.png', bbox_extra_artists=(legend1, legend2), bbox_inches='tight')
