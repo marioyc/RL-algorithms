b4b702ff7e99ed1915de9def5efe9334e0eef1bc
Mario Ynocente Castro <ycmario@gmail.com>
2017-01-03 05:34:00 +0100

    move process function out of the class

--- common/feature_extractors.py
+++ common/feature_extractors.py
@@ -13,6 +13,46 @@ def _pickle_method(m):
 
 copy_reg.pickle(types.MethodType, _pickle_method)
 
+def processBPROSLoop(numRows, numColumns, c1, whichColors):
+    numRowOffsets = 2 * numRows - 1
+    numColumnOffsets = 2 * numColumns - 1
+    numOffsets = numRowOffsets * numColumnOffsets
+    numColorPairs = (1 + 128) * 128 / 2
+    numBasicFeatures = 128 * numRows * numColumns
+    bproExistence = np.ones((numRows * 2 - 1,numColumns * 2 - 1), bool)
+    features = []
+
+    for k in range(len(whichColors[c1])):
+        for h in range(len(whichColors[c1])):
+            rowDelta = whichColors[c1][k][0] - whichColors[c1][h][0]
+            columnDelta = whichColors[c1][k][1] - whichColors[c1][h][1]
+            newBproFeature = False
+            if rowDelta > 0:
+                newBproFeature = True
+            elif rowDelta == 0 and columnDelta >= 0:
+                newBproFeature = True
+            rowDelta += numRows - 1
+            columnDelta += numColumns - 1
+            if newBproFeature and bproExistence[rowDelta][columnDelta]:
+                bproExistence[rowDelta][columnDelta] = False
+                features.append(numBasicFeatures + (128+128-c1+1) * c1/2 * numRowOffsets * numColumnOffsets + rowDelta * numColumnOffsets + columnDelta)
+
+    bproExistence.fill(True)
+
+    for i in range(whichColors.keys().index(c1)+1, len(whichColors.keys())):
+        c2 = whichColors.keys()[i]
+        for it1 in range(len(whichColors[c1])):
+            for it2 in range(len(whichColors[c2])):
+                rowDelta = whichColors[c1][it1][0] - whichColors[c2][it2][0] + numRows - 1
+                columnDelta = whichColors[c1][it1][1] - whichColors[c2][it2][1] + numColumns - 1
+                if bproExistence[rowDelta][columnDelta]:
+                    bproExistence[rowDelta][columnDelta] = False
+                    features.append(numBasicFeatures + (128+128-c1+1) * c1/2 * numRowOffsets * numColumnOffsets + (c2-c1) * numRowOffsets * numColumnOffsets + rowDelta * numColumnOffsets + columnDelta)
+
+        bproExistence.fill(True)
+
+    return features
+
 class AtariFeatureExtractor(object):
     def __init__(self, mode='basic', background=None, parallel=False):
         self.features = set([-1])
@@ -47,51 +87,12 @@ class AtariFeatureExtractor(object):
         self.features = set(self.features)
         self.whichColors = collections.OrderedDict(sorted(self.whichColors.items()))
 
-    def processBPROSLoop(self, c1):
-        numRowOffsets = 2 * self.numRows - 1
-        numColumnOffsets = 2 * self.numColumns - 1
-        numOffsets = numRowOffsets * numColumnOffsets
-        numColorPairs = (1 + 128) * 128 / 2
-        numBasicFeatures = 128 * self.numRows * self.numColumns
-        bproExistence = np.ones((self.numRows * 2 - 1,self.numColumns * 2 - 1), bool)
-        features = []
-
-        for k in range(len(self.whichColors[c1])):
-            for h in range(len(self.whichColors[c1])):
-                rowDelta = self.whichColors[c1][k][0] - self.whichColors[c1][h][0]
-                columnDelta = self.whichColors[c1][k][1] - self.whichColors[c1][h][1]
-                newBproFeature = False
-                if rowDelta > 0:
-                    newBproFeature = True
-                elif rowDelta == 0 and columnDelta >= 0:
-                    newBproFeature = True
-                rowDelta += self.numRows - 1
-                columnDelta += self.numColumns - 1
-                if newBproFeature and bproExistence[rowDelta][columnDelta]:
-                    bproExistence[rowDelta][columnDelta] = False
-                    features.append(numBasicFeatures + (128+128-c1+1) * c1/2 * numRowOffsets * numColumnOffsets + rowDelta * numColumnOffsets + columnDelta)
-
-        bproExistence.fill(True)
-
-        for i in range(self.whichColors.keys().index(c1)+1, len(self.whichColors.keys())):
-            c2 = self.whichColors.keys()[i]
-            for it1 in range(len(self.whichColors[c1])):
-                for it2 in range(len(self.whichColors[c2])):
-                    rowDelta = self.whichColors[c1][it1][0] - self.whichColors[c2][it2][0]+self.numRows-1
-                    columnDelta = self.whichColors[c1][it1][1] - self.whichColors[c2][it2][1]+self.numColumns-1
-                    if bproExistence[rowDelta][columnDelta]:
-                        bproExistence[rowDelta][columnDelta] = False
-                        features.append(numBasicFeatures + (128+128-c1+1) * c1/2 * numRowOffsets * numColumnOffsets + (c2-c1) * numRowOffsets * numColumnOffsets + rowDelta * numColumnOffsets + columnDelta)
-
-            bproExistence.fill(True)
-
-        return features
-
     def extractBPROSFeatures(self, state):
         self.features = list(self.features)
 
         if self.parallel:
-            ret = Parallel(n_jobs=-1)(delayed(self.processBPROSLoop)(c1) for c1 in self.whichColors.keys())
+            ret = Parallel(n_jobs=2)(
+                    delayed(processBPROSLoop)(self.numRows, self.numColumns, c1, self.whichColors) for c1 in self.whichColors.keys())
             for x in ret:
                 self.features += x
         else:
